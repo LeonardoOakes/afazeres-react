b9f790145e9e3d03226e91a16805c230be1b5082
Leonardo Oakes <leonardo.oakes@gmail.com>
2022-02-07 12:15:02 -0300

    Componentes iniciais

---
+++ src/Afazeres.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./Afazeres.css";
+
+function Afazeres() {
+  const [text, setText] = useState("");
+
+  function handleChange(event) {
+    let t = event.target.value;
+    setText(t);
+  }
+
+  return (
+    <div className="container">
+      <h1>Afazeres</h1>
+      <form>
+        <input onChange={handleChange} type="text"></input>
+        <button>Adicionar</button>
+      </form>
+      <ul>
+        <li>{text}</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Afazeres;
