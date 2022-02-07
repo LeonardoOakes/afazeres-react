f0ea1337aad7f84e64cde77d137dd5715286ed05
Leonardo Oakes <leonardo.oakes@gmail.com>
2022-02-07 13:15:57 -0300

    Componentes separados e desmembrados

--- src/Afazeres.js
+++ src/Afazeres.js
@@ -1,36 +1,21 @@
 import React, { useState } from "react";
+import List from "./List";
+import Form from "./Form";
 import "./Afazeres.css";
 
 function Afazeres() {
-  const [text, setText] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    let t = event.target.value;
-    setText(t);
-  }
-
-  function addItem(event) {
-    event.preventDefault();
-    if (text) {
-      setItems([...items, text]);
-      setText("");
-    }
+  function onAddItem(item) {
+    setItems([...items, item]);
   }
 
   return (
     <div className="container">
       <h1>Afazeres</h1>
-      <form>
-        <input onChange={handleChange} type="text" value={text}></input>
-        <button onClick={addItem}>Adicionar</button>
-      </form>
 
-      <ul>
-        {items.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
+      <Form onAddItem={onAddItem}></Form>
+      <List items={items}></List>
     </div>
   );
 }
