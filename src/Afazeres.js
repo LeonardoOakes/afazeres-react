3029f0c5e982599ceb6925039848a2cb649844b7
Leonardo Oakes <leonardo.oakes@gmail.com>
2022-02-07 12:52:57 -0300

    Botão adicionar com  onClick

--- src/Afazeres.js
+++ src/Afazeres.js
@@ -3,21 +3,33 @@ import "./Afazeres.css";
 
 function Afazeres() {
   const [text, setText] = useState("");
+  const [items, setItems] = useState([]);
 
   function handleChange(event) {
     let t = event.target.value;
     setText(t);
   }
 
+  function addItem(event) {
+    event.preventDefault();
+    if (text) {
+      setItems([...items, text]);
+      setText("");
+    }
+  }
+
   return (
     <div className="container">
       <h1>Afazeres</h1>
       <form>
-        <input onChange={handleChange} type="text"></input>
-        <button>Adicionar itens a lista</button>
+        <input onChange={handleChange} type="text" value={text}></input>
+        <button onClick={addItem}>Adicionar</button>
       </form>
+
       <ul>
-        <li>{text}</li>
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
       </ul>
     </div>
   );
